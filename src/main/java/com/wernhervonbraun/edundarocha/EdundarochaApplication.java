ee45c28bf2a9d1287d054ad7ad31b60acf7eb2ca
enrocha0312 <enrocha0312@gmail.com>
2024-04-02 11:53:01 -0300

    comeco de projeto

---
+++ src/main/java/com/wernhervonbraun/edundarocha/EdundarochaApplication.java
@@ -0,0 +1,13 @@
+package com.wernhervonbraun.edundarocha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdundarochaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdundarochaApplication.class, args);
+	}
+
+}
